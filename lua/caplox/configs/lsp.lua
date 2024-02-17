70993435e21ad66d8d940c39d9fce04a6d993334
dmmqz <dmmc@tuta.io>
2024-02-17 22:55:53 +0100

    Change pyright python interpreter to the one in debugpy venv

--- lua/caplox/configs/lsp.lua
+++ lua/caplox/configs/lsp.lua
@@ -26,7 +26,16 @@ require('mason-lspconfig').setup({
             local lua_opts = lsp_zero.nvim_lua_ls()
             require('lspconfig').lua_ls.setup(lua_opts)
         end,
-    }
+        pyright = function()
+            require('lspconfig').pyright.setup({
+                settings = {
+                    python = {
+                        pythonPath = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python",
+                    },
+                },
+            })
+        end,
+    },
 })
 
 local cmp = require('cmp')
