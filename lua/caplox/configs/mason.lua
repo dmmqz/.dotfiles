903e27f74b45c8ef26cc3142a55c33e5fa0e92bf
dmmqz <dmmc@tuta.io>
2024-02-17 03:18:40 +0100

    Add black to auto-installed Mason packages

--- lua/caplox/configs/mason.lua
+++ lua/caplox/configs/mason.lua
@@ -1,6 +1,7 @@
 -- This does autoinstallation of mason packages
 require("mason-tool-installer").setup {
     ensure_installed = {
+        "black",    -- Python formatter
         "debugpy",  -- Python debugger
         "codelldb", -- C++ debugger
     },
