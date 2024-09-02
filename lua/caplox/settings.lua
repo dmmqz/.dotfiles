bdd61799d05e5eee0ec901e3ca3c3b63fef51580
caplox <dmmc@tuta.io>
2024-09-02 23:04:11 +0200

    Create autocmd for 2 space indent for Haskell

--- lua/caplox/settings.lua
+++ lua/caplox/settings.lua
@@ -3,6 +3,15 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
+vim.autoindent = true
+vim.smartindent = true
+
+
+-- Haskell 2 space indent
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "haskell",
+    command = "setlocal tabstop=2 shiftwidth=2 expandtab"
+})
 
 -- Relative line number
 vim.opt.number = true
