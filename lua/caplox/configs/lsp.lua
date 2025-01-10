0ea36c0e963d8b625bc0822bfd6d73086e945181
dmmqz <dmmc@tuta.io>
2025-01-10 23:44:23 +0100

    make own luasnip snippet

--- lua/caplox/configs/lsp.lua
+++ lua/caplox/configs/lsp.lua
@@ -61,7 +61,6 @@ local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
 -- this is the function that loads the extra snippets to luasnip
 -- from rafamadriz/friendly-snippets
-require('luasnip.loaders.from_vscode').lazy_load()
 cmp.setup({
     sources = {
         { name = 'path' },
