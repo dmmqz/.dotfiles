c15e9fefcdc3c5f99e2127be3979fca44f6322ef
dmmqz <dmmc@tuta.io>
2025-07-26 20:00:22 +0200

    zsh: add default argument to screen alias

--- .config/zsh/aliases.sh
+++ .config/zsh/aliases.sh
@@ -2,4 +2,4 @@
 
 alias doas=sudo
 alias sd="systemctl poweroff"
-alias record="ffmpeg -f x11grab -framerate 30 -i :0.0 -c:v libx264 "
+alias record="ffmpeg -f x11grab -framerate 30 -i :0.0 -c:v libx264 ${filename:-output.mp4}"
