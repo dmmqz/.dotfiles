8a813c06344920e8fdff4e9aff606fe7957dee65
dmmqz <dmmc@tuta.io>
2025-08-04 14:17:01 +0200

    zsh: add yayf alias

--- .config/zsh/aliases.sh
+++ .config/zsh/aliases.sh
@@ -3,3 +3,6 @@
 alias doas=sudo
 alias sd="systemctl poweroff"
 alias record="ffmpeg -f x11grab -framerate 30 -i :0.0 -c:v libx264 ${filename:-output.mp4}"
+
+# Find yay packages without leaving the terminal
+alias yayf="yay -Slq | fzf --multi --preview 'yay -Sii {1}' --preview-window=down:75% | xargs -ro yay -S"
