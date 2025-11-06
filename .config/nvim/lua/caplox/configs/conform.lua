49d12cd19b86c79315fa956e3e369dfc2354d261
dmmqz <dmmc@tuta.io>
2025-11-06 16:38:10 +0100

    nvim: add HTML formatter, bind gq to format with conform

--- .config/nvim/lua/caplox/configs/conform.lua
+++ .config/nvim/lua/caplox/configs/conform.lua
@@ -1,7 +1,13 @@
 require("conform").setup({
     formatters_by_ft = {
         python = { "isort", "black" },
+        html = { "prettier" },
     },
 })
 
 vim.keymap.set("n", "<leader>t", require("conform").format)
+vim.keymap.set("n", "gq", function()
+    require("conform").format({ async = true })
+end, { desc = "Format with conform" })
+
+vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
