6da055a13a31f4bb289944ae62aec1579e103a01
dmmqz <dmmc@tuta.io>
2026-02-03 22:17:47 +0100

    zsh: add global aliases and run-help loading

--- .config/zsh/aliases.sh
+++ .config/zsh/aliases.sh
@@ -4,14 +4,20 @@ alias doas=sudo
 alias sd="systemctl poweroff"
 alias record="ffmpeg -f x11grab -framerate 30 -i :0.0 -c:v libx264 ${filename:-output.mp4}"
 
+# Global aliases
+alias -g H='| head'
+alias -g T='| tail'
+alias -g L='| $PAGER'
+alias -g G='| grep'
+alias -g W='| wc -l'
+alias -g C="| xclip -selection clipboard"
+
 # Find yay packages without leaving the terminal
 alias yayf="yay -Slq | fzf --multi --preview 'yay -Sii {1}' --preview-window=down:75% | xargs -ro yay -S"
 alias yayr="yay -Qqe | fzf --multi --preview 'yay -Qi {1}' --preview-window=down:75% | xargs -ro sudo pacman -Rncs --noconfirm"
 
 alias vi=nvim
 alias vim=nvim
-
-alias pass=keepassxc-cli
 
 # Git aliases
 alias gdn="git diff --name-only"
