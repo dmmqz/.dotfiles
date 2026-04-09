c09714136b41ec3f52ccd944f8775fd589016cf2
dmmqz <dmmc@tuta.io>
2026-04-09 16:56:16 +0200

    nvim: add autocommand to update treesitter, add new lock file to gitignore

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,3 +1,14 @@
+-- Automatically update Treesitter on pack update
+vim.api.nvim_create_autocmd('PackChanged', {
+    callback = function(ev)
+        local name, kind = ev.data.spec.name, ev.data.kind
+        if name == 'nvim-treesitter' and kind == 'update' then
+            if not ev.data.active then vim.cmd.packadd('nvim-treesitter') end
+            vim.cmd('TSUpdate')
+        end
+    end
+})
+
 local gh = function(x) return 'https://github.com/' .. x end
 
 vim.pack.add({
